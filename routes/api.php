ec4decfaca1b6dbf49acf59be23541a295e694ff
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-02-08 01:18:41 +0330

    Customized app js file.

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use \App\Http\Controllers\Api\GameController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('startNewGame', [GameController::class, 'startNewGame']);
+
+});
+
+
+
+
