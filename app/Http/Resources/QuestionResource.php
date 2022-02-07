b09ac49af4164bb41379fbde47277475161fc885
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-02-08 01:20:22 +0330

    Created a new api resource for question api

---
+++ app/Http/Resources/QuestionResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class QuestionResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+       // return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'point' => $this->point
+        ];
+    }
+}
