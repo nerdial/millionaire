96e463b690c22836a7ad4e07ab98d0f11a0bba3f
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-02-08 01:16:23 +0330

    Initialized a new component for start game functionality

---
+++ resources/js/components/StartGameComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Would you like to start a fresh game ?</div>
+                    <div class="card-body">
+                        <button @click="startNewGame" type="button" class="btn btn-primary">Start</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+
+        },
+        methods : {
+            startNewGame(){
+
+                axios.get('api/startNewGame').then(data=> {
+                    console.log(data)
+                })
+
+            }
+        }
+    }
+</script>
