517cff71b1e48cdcc6870d04f3d5cb7d2c74c0e6
Vahid Mahdiun <v.mahdiun@yahoo.com>
2022-02-09 01:02:53 +0330

    Added new routes for register and login

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Api\GameController;
+use \App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,11 +15,18 @@ use \App\Http\Controllers\Api\GameController;
 |
 */
 
+Route::post('/register', [AuthController::class, 'register']);
+//login user
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/out', [AuthController::class, 'logout']);
+
 
     Route::get('startNewGame', [GameController::class, 'startNewGame'])
         ->name('api.game.start');
